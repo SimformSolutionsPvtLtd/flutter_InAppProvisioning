256135d564a2fc755365fe008273e05eb0c96b79
sanket-simform <sanket.k@simformsolutions.com>
2022-03-11 11:14:31 +0530

    ♻️ Improved swift code.

--- ios/Classes/SwiftInAppProvisioningPlugin.swift
+++ ios/Classes/SwiftInAppProvisioningPlugin.swift
@@ -2,31 +2,29 @@ import PassKit
 import Flutter
 import UIKit
 
-var channel: FlutterMethodChannel?
-
 public class SwiftInAppProvisioningPlugin: NSObject, FlutterPlugin {
+    
+    var channel: FlutterMethodChannel?
     
     let inAppProvisioning:InAppProvisioningEnrollment = InAppProvisioningEnrollment()
     
   public static func register(with registrar: FlutterPluginRegistrar) {
     let _channel = FlutterMethodChannel(name: CHANNEL_NAME, binaryMessenger: registrar.messenger())
     
-    channel = _channel
     
     let instance = SwiftInAppProvisioningPlugin()
+    instance.channel = _channel
     registrar.addMethodCallDelegate(instance, channel: _channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    
-    
     if(call.method == IS_PASSKIT_AVAILABLE) {
         result(inAppProvisioning.isPassKitAvailable())
     } else if(call.method == INIT_ENROLLMENT) {
         guard let args = call.arguments as? [String : Any] else {return}
         let panToken = args["panTokenSuffix"] as! String
         let name = args["holderName"] as! String
-        inAppProvisioning.initEnrollProcess(panTokenSuffix: panToken, holderName: name)
+        inAppProvisioning.initEnrollProcess(panTokenSuffix: panToken, holderName: name, methodChannel: channel!)
         result(["" : ""])
     }
   }
